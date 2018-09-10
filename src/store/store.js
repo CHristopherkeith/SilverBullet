bc94d87e310ceaad26b54133bb5156ce5b9e35f1
Christopher <easonmu@hotmail.com>
2018-09-10 23:35:01 +0800

    try to call contract method to get data and get result by txhash

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,14 @@ import * as types from "./type.js"
 import NebPay from 'nebpay.js'
 import Nebulas from 'nebulas'
 
+NebPay.config = {
+	testnetUrl: "https://testnet.nebulas.io",
+	mainnetUrl: "https://mainnet.nebulas.io"
+}
 const contractAddress = 'n21Rvxijhp9u8ubkWYotFPDnWLfGnpWgXSy';
 const neb = new Nebulas.Neb();
 const chainID = 1001;
+const nebPay = new NebPay();
 neb.setRequest(new Nebulas.HttpRequest("https://testnet.nebulas.io"));
 // neb.setRequest(new Nebulas.HttpRequest("https://mainnet.nebulas.io"));
 
@@ -80,7 +85,7 @@ const store = new Vuex.Store({
 			// return new Promise((resolve, reject) => {
 			// 	neb.api.getAccountState(state.userAddress)
 			// 	.then(
-			// 		res => {return dispatch('GET_STORE', res)},
+			// 		res => {dispatch('GET_STORE', res)},
 			// 		err => {reject(err);}
 			// 	).then(
 			// 		res => {resolve(res);},
@@ -92,7 +97,7 @@ const store = new Vuex.Store({
 	        // 链式写法：多个then放在GET_ACCOUNT_STATE【外】
 	        /********************************************/ 
 			return new Promise((resolve, reject) => {
-				neb.api.getAccountState('state.userAddress')
+				neb.api.getAccountState(state.userAddress)
 				.then(
 					res => {resolve(res)},
 					err => {reject(err);}
@@ -100,28 +105,78 @@ const store = new Vuex.Store({
 			})
 		},
 		[types.GET_STORE]({commit, state}, res){
-			return new Promise((resolve, reject) => {
-				neb.api.call({
-	            	chainID,
-	            	from: state.userAddress,
-	            	to: contractAddress,
-	            	value: 0,
-	            	nonce: parseInt(res.nonce) + 1,
-	            	gasPrice: 1000000,
-				   	gasLimit: 2000000,
-				   	contract: {
-				       function: "balanceOf",
-				       args: "[0]"
-				   }
-	            }).then(
-	            	res => {
-	            		let result = JSON.parse(res.result || res);
-	            		resolve(result);
-	            	},
-	            	err => {
-						reject(err);
-					}
-	            )
+			// 使用call方法（不花费手续费）
+			// return new Promise((resolve, reject) => {
+			// 	neb.api.call({
+	  //           	chainID,
+	  //           	from: state.userAddress,
+	  //           	to: contractAddress,
+	  //           	value: 0,
+	  //           	nonce: parseInt(res.nonce) + 1,
+	  //           	gasPrice: 1000000,
+			// 	   	gasLimit: 2000000,
+			// 	   	contract: {
+			// 	       function: "balanceOf",
+			// 	       args: "[0]"
+			// 	   }
+	  //           }).then(
+	  //           	res => {
+	  //           		let result = JSON.parse(res.result || res);
+	  //           		resolve(result);
+	  //           	},
+	  //           	err => {
+			// 			reject(err);
+			// 		}
+	  //           )
+			// })
+
+			// 使用直接向合约发送数据的方法（花费手续费）
+			let param = 0;
+			let callRes = nebPay.call(contractAddress, 0, 'balanceOf', `[${param}]`, {
+				qrcode: {
+	                showQRCode: false
+	            },
+	   //          extension: {
+				// 	openExtension: true //是否支持插件调用
+				// },
+	            callback: NebPay.config.testnetUrl,
+	            listener: function(serialNumber, result){
+	            	console.log(serialNumber, '【serialNumber】')
+	            	console.log(result, '【result】')
+
+	            	const intervalQuery = setInterval(function() {
+			            funcIntervalQuery();
+			        }, 5000);
+
+	            	function funcIntervalQuery() {  
+	            		// 使用nebpay接口 
+				        // nebPay.queryPayInfo(serialNumber, {callback: NebPay.config.testnetUrl})
+			         //    .then(
+			         //    	res => {
+				        //         let respObject = JSON.parse(res)
+				        //         console.log(respObject, '【respObject111】')
+				        //         if(respObject.code === 0 && respObject.data.status === 1){                    
+				        //             clearInterval(intervalQuery)
+				        //         }
+			         //    	},
+			         //    	err => {console.log(err, '【err222】')}
+			         //    )
+
+			         	// 使用nebjs接口查询
+			         	neb.api.getTransactionReceipt(result.txhash)
+			         	.then(
+			         		res => {
+			         			console.log(res)
+			         			if(res.status === 1){
+			         				console.log('clearInterval')
+			         				clearInterval(intervalQuery)
+			         			}
+			         			
+			         		},
+			         		err => {console.log(err, '【err222】')}
+			         	)
+				    }
+	            }
 			})
 		}
 	}
